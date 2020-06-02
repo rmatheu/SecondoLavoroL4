a602dc115a652d098b52ae89d7b755a8cfc042c6
rmatheu <santmatheu23@gmail.com>
2020-06-01 18:41:03 -0600

    edit 9

---
+++ Ejer4.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1> Ejercicio 4</h1>
+
+<?php
+
+header  ("Content-type: text/html;charset=\"utf-8\"");
+
+$usuario = "rmatheu";
+
+if ($usuario=="rmatheu" || $usuario =="rmtaheu")
+{
+    echo "<h1>Usuario es valido<h/h1>";
+}
+else
+{
+    echo "<h1>Lo siento $usuario, no tiene acceso a las opciones</h1>";
+}
+
+$edad = 15;
+if($edad >=18)
+{
+    echo "<h1> Tiene la edad requerida</h1>";
+}
+else
+{
+    echo "<h1>Lo siento debe tener al menos 18 años</h1>";
+}
+
+?>
+
+</body>
+</html>
