fe9a6247ca90e72a333c0ddd0fde896abfa95211
rmatheu <santmatheu23@gmail.com>
2020-06-01 18:05:44 -0600

    edit 5

---
+++ Ejer3.php
@@ -0,0 +1,39 @@
+!DOCTYPE html>
+<html>
+<body>
+
+<h1> Ejercicio 3</h1>
+
+<?php
+
+echo  "<h2>Arreglos</h2>";
+
+$miArreglo = array (10, 20, 30, 40, 50);
+
+echo "<p>Contenido del elemento 2 del arreglo: $miArreglo[2]</p>";
+
+echo "<p>Despliega el contenido del arreglo:</p>";
+print_r ($miArreglo);
+
+echo "<p>El tamaño del arreglo es:" .sizeof($miArreglo). "</p>";
+
+$miFechaArr["dia"]="Lunes";
+$miFechaArr["mes"]="Mayo";
+$miFechaArr["año"]="2020";
+
+echo "<br><br>>";
+
+var_dump ($miFechaArr["año"]);
+
+unset   ($miFechaArr["año"]);
+echo "<br><br>";
+print_r ($miFechaArr);
+$MiArregloReves =    array_reverse($miArreglo);
+
+echo "<br><br>"
+print_r($MiArregloReves);
+
+?>
+
+</body>
+</html>
