4e3d9fceefab86f6384f498b601be8138929e0e7
rmatheu <santmatheu23@gmail.com>
2020-06-01 19:06:53 -0600

    edit 10

---
+++ Ejer6.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1> Ejercicio 6</h1>
+
+<?php
+
+header  ("Content-type: text/html;charset=\"utf-8\"");
+
+echo "<h1>".$_GET['clase']."</h1>";
+echo "<h2>".$_GET['periodo']."</h2>";
+
+echo "<h1>".$_GET['nombre']."</h1>";
+echo "<h2>".$_GET['apellido']."</h2>";
+
+if (is_numeric($_GET['edad']) && $_GET['edad'] >=1)
+{
+    echo "<h3>Bienvenido ".$_GET['nombre']." ".$_GET['apellido']."</h3>";
+}
+else{
+    echo "<h3>No es una edad valida.</h3>";
+}
+
+
+?>
+
+<form>
+    Escribe tu nombre completo:
+    <input name="nombre" type="text" placeholder="Escribe nombres">
+    <input name="apeliido" type="text" placeholder="Escribe apellidos">
+    <input name="edad" type="text" placeholder="Escribe edad">
+    <input type="submit"  value="Registrar">
+</form>
+
+</body>
+</html>
