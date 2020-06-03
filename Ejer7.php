09d78c512866f3e45dc719958704f236b2da4de8
rmatheu <santmatheu23@gmail.com>
2020-06-02 21:13:32 -0600

    Edit 16

--- Ejer7.php
+++ Ejer7.php
@@ -4,9 +4,9 @@
 
 <h1> Ejercicio 7</h1>
 <form>
-    Escribe un numero:
-    <input name="Numero" type="numeric" placeholder="Escribe numero">
-    <input type="submit"  value="R">
+    Escriba un numero:
+    <input name="Numero" type="text" placeholder="Escriba un numero">
+    <input type="submit"  value="Revisar">
 </form>
 
 <?php
@@ -14,12 +14,24 @@
 header  ("Content-type: text/html;charset=\"utf-8\"");
 
 
-
+if (is_numeric($_GET['Numero']))
+{
+    if($_GET['Numero']>1)
+    {
+        echo "<h2>Numero ingreso valido</h2>";
+    }
+    else
+    {
+        echo "<h2>Numero ingresado no valido, es igual o menor a 1</h2>";
+   }
+}
+    else 
+    {
+        echo "<h2>Ingreso no valido</h2>";
+    }
 
 
 ?>
-
-
 
 </body>
 </html>
